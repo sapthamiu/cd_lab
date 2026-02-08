70e9fef3b145c18e8a422c999b3a7551cb6eeb7e
Sapthami <sapthami.mitmpl2023@learner.manipal.edu>
2026-02-08 09:24:04 +0530

    Lab5

---
+++ Lab5/samplecom.c
@@ -0,0 +1,22 @@
+#include <STDIO.H>
+
+/* THIS IS A MULTI-LINE COMMENT
+   WITH UPPERCASE LETTERS
+   AND symbols like A+B, X*Y
+*/
+
+int MAIN()
+{
+    int A = 10;
+    int B = 20;
+
+    // SINGLE LINE COMMENT WITH CAPS
+    printf("HELLO WORLD\n");
+    printf("SUM = %d\n", A + B);
+
+    /* Another COMMENT
+       MixedCase COMMENT
+    */
+
+    RETURN 0;
+}
